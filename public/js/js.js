b27fd66f654d61bffdeacbfbc328cabb9d5e5cc0
PedroAldama <pedro.aldama@uan.edu.mx>
2021-08-13 10:09:19 -0600

    The function Scroll on nav was added, and the javaScript code was added

---
+++ public/js/js.js
@@ -0,0 +1,13 @@
+
+    window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+  
+  } else {
+    document.getElementById("navbar").style.padding = "40px 10px";
+
+  }
+}
